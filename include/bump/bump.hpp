f374d87f4173cc84630d6764eeb8202142cde6b0
Paulo Morales <pajemorales@gmail.com>
2019-12-10 15:25:18 +0900

    add function to check intersection of boxes

--- include/bump/bump.hpp
+++ include/bump/bump.hpp
@@ -12,6 +12,8 @@ struct Vector2d {
      */
     Vector2d operator+(const Vector2d& rhs) const;
     Vector2d operator-(const Vector2d& rhs) const;
+    Vector2d operator*(double factor) const;
+    Vector2d operator/(double divisor) const;
     double dot(const Vector2d& rhs) const;
 };
 
@@ -19,7 +21,28 @@ struct Box2d {
     Vector2d center;     // center of the box
     Vector2d dim;        // dimensions x & y
     double orientation;  // orientation from x-axis in radians
+
+    /**
+     * @brief Check intersection with another 2D box.
+     *
+     * @param other box
+     * @param margin to extend this box's dimensions at both ends
+     * @return true if the boxes intersect, false otherwise
+     */
+    bool intersects(const Box2d other, double margin = 0.0) const;
 };
+
+namespace utils
+{
+/**
+ * @brief Create a unit vector with the giver orientation.
+ *
+ * @param orientation from x-axis in radians
+ * @return Vector2d unit vector
+ */
+Vector2d createUnitVector(double orientation);
+
+}  // namespace utils
 
 }  // namespace bump
 
--- src/bump.cpp
+++ src/bump.cpp
@@ -1,3 +1,6 @@
+#include <cmath>
+#include <vector>
+
 #include "bump/bump.hpp"
 
 namespace bump
@@ -16,5 +19,67 @@ double Vector2d::dot(const Vector2d& rhs) const
 {
     return (this->x * rhs.x) + (this->y * rhs.y);
 }
+
+Vector2d Vector2d::operator*(double factor) const
+{
+    return {this->x * factor, this->y * factor};
+}
+
+Vector2d Vector2d::operator/(double divisor) const
+{
+    return {this->x / divisor, this->y / divisor};
+}
+
+bool Box2d::intersects(const Box2d other, double margin) const
+{
+    // reference implementation from
+    // https://www.jkh.me/files/tutorials/Separating%20Axis%20Theorem%20for%20Oriented%20Bounding%20Boxes.pdf
+
+    // generate the candidate separating axes
+    std::vector<Vector2d> candidateSeparatingAxes;
+    Vector2d thisLocalX = utils::createUnitVector(this->orientation);
+    Vector2d thisLocalY = {thisLocalX.y, -thisLocalX.x};
+    candidateSeparatingAxes.push_back(thisLocalX);
+    candidateSeparatingAxes.push_back(thisLocalY);
+    Vector2d otherLocalX = utils::createUnitVector(other.orientation);
+    Vector2d otherLocalY = {otherLocalX.y, -otherLocalX.x};
+    candidateSeparatingAxes.push_back(otherLocalX);
+    candidateSeparatingAxes.push_back(otherLocalY);
+
+    // check if the candidate axis is a separating axis
+    auto isAxisSeparating = [&](const Vector2d& axis) {
+        Vector2d centerToCenter = other.center - this->center;
+        double centerToCenterProj = centerToCenter.dot(axis);
+
+        double thisHalfDimX = (this->dim.x / 2.0) + margin;
+        double thisHalfDimY = (this->dim.y / 2.0) + margin;
+        double thisHalfDimXProj = std::abs((thisLocalX * thisHalfDimX).dot(axis));
+        double thisHalfDimYProj = std::abs((thisLocalY * thisHalfDimY).dot(axis));
+
+        double otherHalfDimX = (other.dim.x / 2.0);
+        double otherHalfDimY = (other.dim.y / 2.0);
+        double otherHalfDimXProj = std::abs((otherLocalX * otherHalfDimX).dot(axis));
+        double otherHalfDimYProj = std::abs((otherLocalY * otherHalfDimY).dot(axis));
+
+        double sumProjections = thisHalfDimXProj + thisHalfDimYProj + otherHalfDimXProj + otherHalfDimYProj;
+        return centerToCenterProj > sumProjections;
+    };
+    for (const Vector2d& axis : candidateSeparatingAxes) {
+        if (isAxisSeparating(axis)) {
+            // the boxes do not intersect if there is at least one separating axis
+            return false;
+        }
+    }
+    return true;
+}
+
+namespace utils
+{
+Vector2d createUnitVector(double orientation)
+{
+    return {std::cos(orientation), std::sin(orientation)};
+}
+
+}  // namespace utils
 
 }  // namespace bump
